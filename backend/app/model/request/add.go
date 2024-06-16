62b20d7964eb389ced8b7a0c9171d4d8e2ce9f87
Kasipat Wongsamran <kasipat.w@arise.tech>
2024-06-16 10:18:14 +0700

    fix typo and db script

--- backend/app/model/request/add.go
+++ backend/app/model/request/add.go
@@ -2,7 +2,7 @@ package request
 
 type Add struct {
 	Title  string `json:"title"`
-	Author string `json:"Author"`
+	Author string `json:"author"`
 	Genre  string `json:"genre"`
 	Year   string `json:"year"`
 	Tag    string `json:"tag"`
